1e93c817e707ca58da9925ac91ee68c1d29240c6
Gurbani Saroy <gurbanisaroy@gmail.com>
2025-05-15 21:47:43 +0530

    product section completed

--- src/landing_page/product/LeftSection.js
+++ src/landing_page/product/LeftSection.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-function LeftSection() {
-    return ( 
-        <h1>left section</h1>
-     );
+function LeftSection({ imageURL, productName, productDesc, tryDemo, LearnMore, googlePlay, appStore }) {
+    return (
+        <div className='container mt-4'>
+            <div className='row'>
+                <div className='col-6 p-3'>
+                    <img src={imageURL} style={{ width: '80%' }} />
+                </div>
+                <div className='col-6  p-3' style={{ margin: 'auto' }}>
+                    <h1>{productName}</h1>
+                    <br></br>
+                    <p style={{ lineHeight: '2em' }}>{productDesc}</p>
+                    <div>
+                        <a href={tryDemo}>Try Demo
+                            <i class="fa-solid fa-arrow-right" aria-hidden='true' /></a>
+                        <a href={LearnMore} style={{ marginLeft: '10em' }}>
+                            Learn More
+                            <i class="fa-solid fa-arrow-right" aria-hidden='true' />
+                        </a>
+                    </div>
+                    <div className='mt-3'>
+                        <a href={googlePlay}>
+                            <img src='../assets/googlePlayBadge.svg' />
+                        </a>
+                        <a href={appStore}>
+                            <img src='../assets/appStoreBadge.svg' style={{ marginLeft: '7em' }} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LeftSection;
--- src/landing_page/product/ProductPage.js
+++ src/landing_page/product/ProductPage.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import Hero from '../product/Hero';
+import LeftSection from './LeftSection';
+import RightSection from './RightSection';
+import Universe from './Universe';
 function ProductPage() {
-    return ( 
-        <>
-        <h1>ProductPage</h1>
-        </>
-     );
+    return (
+        <div className='p-5 m-5'>
+            <Hero />
+            <hr/>
+            <LeftSection imageURL="../assets/kite.png"
+                productName="Kite"
+                productDesc="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+                tryDemo=""
+                learnMore=""
+                googlePlay=""
+                appStore=""
+            />
+            <RightSection
+                imageURL="../assets/console.png"
+                productName="Console"
+                productDesc="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+                learnMore="" />
+            <LeftSection
+                imageURL="../assets/coin.png"
+                productName="Coin"
+                productDesc="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+                tryDemo=""
+                learnMore=""
+                googlePlay=""
+                appStore=""
+            />
+            <RightSection
+                imageURL="../assets/kiteconnect.png"
+                productName="Kite Connect API"
+                productDesc="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+                learnMore=""
+            />
+            <LeftSection
+                imageURL="../assets/varsity.png"
+                productName="Varsity mobile"
+                productDesc="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+                tryDemo=""
+                learnMore=""
+                googlePlay=""
+                appStore=""
+            />
+            <Universe />
+        </div>
+    );
 }
 
 export default ProductPage;
